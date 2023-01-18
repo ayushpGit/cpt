3709593b810ee5ba6193785f72e5f740e269db86
kritydangol <kritydangol1@gmail.com>
2023-01-18 16:36:04 +0545

    upload asks

---
+++ asks/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="development">Development</div>
+      <div className="designing">Designing</div>
+      <div className="marketing">Marketing</div>
+    </div>
+  );
+}
+
+export default App;
