2a0c3994bb1a75a9595ef0a77387d0d03164f3a8
kritydangol <kritydangol1@gmail.com>
2023-01-19 18:27:13 +0545

    enhanced structure, worked on home page, left: css div before

--- asks/src/App.js
+++ asks/src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="development">Development</div>
-      <div className="designing">Designing</div>
-      <div className="marketing">Marketing</div>
+      
+      <div className="home-development">
+        <p id="home-animate-text1">Development</p>
+      </div>
+      <div className="home-designing">
+        <p id="home-animate-text2">Designing</p>
+      </div>
+      <div className="home-marketing">
+        <p id="home-animate-text3">Marketing</p>
+      </div>
     </div>
   );
 }
---
+++ asks/src/Pages/home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <div className="development">Development</div>
+      <div className="designing">Designing</div>
+      <div className="marketing">Marketing</div>
+    </div>
+  );
+};
+
+export default home;
