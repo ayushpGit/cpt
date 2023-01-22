4176ee9953fcd4778e56d1122923c4c8b060d57a
kritydangol <kritydangol1@gmail.com>
2023-01-22 18:11:19 +0545

    home page enhanced: images added, animation sorted

--- asks/src/App.js
+++ asks/src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-
+import develop from "./assets/images/development.jpg";
+import design from "./assets/images/design.jpg";
+import market from "./assets/images/marketing.jpg";
 function App() {
   return (
     <div className="App">
-      
       <div className="home-development">
         <p id="home-animate-text1">Development</p>
+        <div className="home-img-wrap">
+          <img className="home-img develop" src={develop} alt="develop"></img>
+        </div>
       </div>
       <div className="home-designing">
         <p id="home-animate-text2">Designing</p>
+        <img className="home-img design" src={design} alt="design"></img>
       </div>
       <div className="home-marketing">
         <p id="home-animate-text3">Marketing</p>
+        <img className="home-img market" src={market} alt="market"></img>
       </div>
+      <div id="overlay"></div>
     </div>
   );
 }
