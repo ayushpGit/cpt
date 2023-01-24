acf72bbb69a04ec07c45b48a39c27f801dae55c0
kritydangol <kritydangol1@gmail.com>
2023-01-24 18:16:18 +0545

    updates: routing, home, nav

---
+++ asks/src/Pages/nav/nav.js
@@ -0,0 +1,14 @@
+import './nav.css'
+
+const nav = () => {
+  return (
+    <div>
+        <p className='nav-cpt'>CPT</p>
+        <a className='nav-a cpt' href='#!'>CPT</a>
+        <a className='nav-a cpd' href='#!'>CPD</a>
+        <a className='nav-a cpm' href='#!'>CPM</a>
+    </div> 
+  )
+}
+
+export default nav
---
+++ asks/src/utils/routing.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// importing pages
+import Nav from "../Pages/nav/nav";
+import Home from "../Pages/home/home";
+
+const Routing = () => {
+  const path = window.location.pathname;
+  return (
+    <Router>
+      {/* body: */}
+      <div className="body">
+        <div className="container">
+          <div className="row">
+            <Routes>
+              {/* home route */}
+              <Route exact path="/" element={<Nav />} />
+              <Route exact path="/" element={<Home />} />
+
+              {/* default routes for invalid links */}
+              {/* <Route path="*" element={<NotFound />} /> */}
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default Routing;
