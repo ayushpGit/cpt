68b9ae03ca80180885f5d3ad9f17cbf555952756
kritydangol <kritydangol1@gmail.com>
2023-02-19 18:18:19 +0545

    minor changes for adding menu

--- asks/src/components/nav.js
+++ asks/src/components/nav.js
@@ -1,12 +1,23 @@
 import "./components-style.css";
+import { CgMenuRight } from "react-icons/cg";
 
 const nav = () => {
   return (
+    <>
     <nav>
-      <span className="nav-crocus">Crocus Pearl</span>
-      <br />
-      <span className="nav-tech">technologies</span>
+      <div className="col-md-6">
+        <p className="nav-crocus">Crocus Pearl</p>
+        {/* <br /> */}
+        <p className="nav-tech">technologies</p>
+      </div>
+      <div className="nav-right">
+        <a className="nav-works" href="#!">
+          Works
+        </a>
+        <CgMenuRight className="nav-menu" />
+      </div>
     </nav>
+    </>
   );
 };
 
--- asks/src/utils/routing.js
+++ asks/src/utils/routing.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // importing pages
-// import Nav from "../components/nav";
+import Nav from "../components/nav";
 import Foot from "../components/footer";
 import Home from "../pages/home";
 import Play from "../pages/playground/playground";
@@ -14,7 +14,7 @@ const Routing = () => {
       <div className="body">
         <Routes>
           {/* home route */}
-          {/* <Route exact path="/nav" element={<Nav />} /> */}
+          <Route exact path="/nav" element={<Nav />} />
           <Route exact path="/" element={<Home />} />
           <Route exact path="/playground" element={<Play />} />
           <Route exact path="/foot" element={<Foot />} />
