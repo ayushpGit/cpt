778d8a03d93db541aeb7f154202fe61889d630b4
mediokrity <kritydangol1@gmail.com>
2023-02-22 18:48:11 +0545

    nav updated

--- asks/src/components/nav.js
+++ asks/src/components/nav.js
@@ -8,6 +8,34 @@ const Nav = () => {
   const toggleOverlay = () => {
     setIsOpen(!isOpen);
   };
+
+
+  const menuItems = [
+    {
+      name: "Home",
+      link: "/"
+    },
+    {
+      name: "Works",
+      link: "/"
+    },
+    {
+      name: "Case Studies",
+      link: "/"
+    },
+    {
+      name: "Expertise",
+      link: "/"
+    },
+    {
+      name: "About",
+      link: "/"
+    },
+    {
+      name: "Contact",
+      link: "/"
+    },
+  ];
   return (
     <>
       <nav>
@@ -29,9 +57,56 @@ const Nav = () => {
             <RxCross2 className="overlay__close" onClick={toggleOverlay} />
           </div>
           <div className="overlay__content">
-            <h1>Contact us</h1>
-            <h1>Careers</h1>
+            <div className="nav-container-1">
+              <p>menu</p>
+            </div>
+
+            <div className="nav-container-2">
+              {menuItems.map((menuItem) => (
+                <a className="menu-items" href={menuItem.link}>{menuItem.name}</a>
+              ))}
+            </div>
+
+            <div className="nav-container-3">
+
+              <p className="nav-address sub-heading">nepal</p>
+              <p className="nav-address">khusibun, kathmandu</p>
+              <a
+                href="mailto:projects@crocuspearl.com"
+                className="nav-email"
+              >
+                project@amiraliadib.com
+              </a>
+              <a href="/" className="nav-privacy">Privacy Policy</a>
+              <div className="nav-socials">
+                <p className="sub-heading">Social</p>
+                <a
+                  href="https://www.facebook.com/people/Crocus-Pearl-Technologies-Pvt-Ltd/100088528600904/?mibextid=ZbWKwL"
+                  // className="link-button"
+                  target={"_blank"}
+                  rel={"noreferrer"}>
+                  Facebook
+                </a>
+                <a
+                  href="https://www.instagram.com/crocus.pearl/"
+                  // className="link-button"
+                  target={"_blank"}
+                  rel={"noreferrer"}>
+                  Instagram
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/crocus-pearl-technologies-pvt-ltd/mycompany/"
+                  // className="link-button"
+                  target={"_blank"}
+                  rel={"noreferrer"}>
+                  Linkedin
+                </a>
+              </div>
+            </div>
           </div>
+
+
+
         </div>
       )}
     </>
