22a00c4d8cba0702471317838d7a27b444cad095
kritydangol <kritydangol1@gmail.com>
2023-03-01 18:13:15 +0545

    playground, cursor

--- asks/src/Pages/playground/playground.js
+++ asks/src/Pages/playground/playground.js
@@ -1,49 +1,114 @@
-import React from "react";
-// import './playground.css';
-class playground extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
+import React, { Component, useState, useEffect } from "react";
+import Slider from "react-slick";
+import motion from "framer-motion";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./playground.css";
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
+const Playground = () => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  console.log(mousePosition);
 
-  // ComponentDidMount is used to
-  // execute the code
-  componentDidMount() {
-    fetch("http://192.168.1.70:3000/users")
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          items: json,
-          DataisLoaded: true,
-        });
+  useEffect(() => {
+    const mouseMove = (e) => {
+      console.log(e);
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
       });
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default:{
+      
+    }
   }
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Pls wait some time.... </h1>{" "}
+
+  const settings = {
+    arrows: false,
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    // centerPadding: "100px",
+    slidesToShow: 1,
+    speed: 500,
+  };
+
+  return (
+    <div>
+      <motion.div className="cursor" variants={variants} />
+      <Slider {...settings}>
+        <div className="carousel-div">
+          <img
+            className="carousel-img"
+            src={require("../../assets/images/img.jpg")}
+            alt="service img"
+          />
+          <div className="container-fluid">
+            <div className="row ">
+              <div className="col-md-1 carousel-num">01</div>
+              <div className="col-md-4">
+                <h2 className="service-title">&lt;Digital Marketing&gt;</h2>
+                <p>
+                  we help individuals understand the effectiveness of their
+                  marketing campaigns, allowing them to make data-driven
+                  decisions
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      );
+        <div className="carousel-div hah">
+          <img
+            className="carousel-img"
+            src={require("../../assets/images/img.jpg")}
+            alt="service img"
+          />
+          <div className="container-fluid">
+            <div className="row ">
+              <div className="col-md-1 carousel-num">02</div>
+              <div className="col-md-4">
+                <h2 className="service-title">&lt;Website Development&gt;</h2>
+                <p>
+                  we help individuals understand the effectiveness of their
+                  marketing campaigns, allowing them to make data-driven
+                  decisions
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-div hah">
+          <img
+            className="carousel-img"
+            src={require("../../assets/images/img.jpg")}
+            alt="service img"
+          />
+          <div className="container-fluid">
+            <div className="row ">
+              <div className="col-md-1 carousel-num">03</div>
+              <div className="col-md-4">
+                <h2 className="service-title">&lt;Digital Marketing&gt;</h2>
+                <p>
+                  we help individuals understand the effectiveness of their
+                  marketing campaigns, allowing them to make data-driven
+                  decisions
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
-    return (
-      <div className="playground">
-        <h1> Fetch data from an api in react </h1>{" "}
-        {items.map((item) => (
-          <ol key={item.id}>
-            User_Name: {item.username}, Full_Name: {item.name}, User_Email:{" "}
-            {item.email}
-          </ol>
-        ))}
-      </div>
-    );
-  }
-}
-
-export default playground;
+export default Playground;
