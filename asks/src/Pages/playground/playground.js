8a54e8678632cddc42a4f3a976e03900127b09d4
kritydangol <kritydangol1@gmail.com>
2023-03-14 18:22:10 +0545

    nav updates

--- asks/src/Pages/playground/playground.js
+++ asks/src/Pages/playground/playground.js
@@ -1,16 +1,7 @@
 import "./playground.css";
-import { CgMenuRight } from "react-icons/cg";
-import { RxCross2 } from "react-icons/rx";
-import { useState } from "react";
+import React from "react";
 
-const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleOverlay = () => {
-    // console.log("Before"+isOpen);
-    setIsOpen(!isOpen);
-    // console.log("After"+isOpen);
-  };
-
+const Playground = () => {
   const menuItems = [
     {
       name: "Home",
@@ -38,88 +29,74 @@ const Nav = () => {
     },
   ];
   return (
-    <>
-      {/* <nav>
-        <div className="col-md-6 nav-logo">
-          <p className="nav-crocus">crocus pearl</p>
-          <p className="nav-tech">technologies</p>
+    <div>
+      <input type="checkbox" id="ham-menu" />
+      <label for="ham-menu">
+        <div class="hide-des">
+          <span class="menu-line"></span>
+          <span class="menu-line"></span>
+          <span class="menu-line"></span>
+          <span class="menu-line"></span>
+          <span class="menu-line"></span>
+          <span class="menu-line"></span>
         </div>
-        <div className="nav-right">
-          <a className="nav-works" href="#!">
-            Works
-          </a>
-          <CgMenuRight className="nav-menu" onClick={toggleOverlay} />
-        </div>
-      </nav> */}
-      <header>
-        <input type="checkbox" id="menuToggle" />
-        <label id="burger" for="menuToggle">
-          <div></div>
-          <div></div>
-          <div></div>
-        </label>
-        <nav id="menuu"></nav>
-      </header>
-      {isOpen && (
-        <div className={isOpen ? "overlay active" : "overlay"}>
-          <div className="overlay__controls">
-            <RxCross2 className="overlay__close" onClick={toggleOverlay} />
+      </label>
+      {/* <!-- <div class="full-page-green"></div> --> */}
+      <div class="ham-menu">
+        <div className="overlay__content">
+          <div className="nav-container-1">
+            <p>menu</p>
           </div>
-          <div className="overlay__content">
-            <div className="nav-container-1">
-              <p>menu</p>
-            </div>
 
-            <div className="nav-container-2">
-              {menuItems.map((menuItem) => (
-                <a className="menu-items" href={menuItem.link}>
-                  {menuItem.name}
-                </a>
-              ))}
-            </div>
+          <div className="nav-container-2">
+            {menuItems.map((menuItem) => (
+              <a className="menu-items" href={menuItem.link}>
+                {menuItem.name}
+              </a>
+            ))}
+          </div>
 
-            <div className="nav-container-3">
-              <p className="nav-address sub-heading">nepal</p>
-              <p className="nav-address">khusibun, kathmandu</p>
-              <a href="mailto:projects@crocuspearl.com" className="nav-email">
-                projects@crocuspearl.com
+          <div className="nav-container-3">
+            <p className="nav-address sub-heading">nepal</p>
+            <p className="nav-address">khusibun, kathmandu</p>
+            <a href="mailto:projects@crocuspearl.com" className="nav-email">
+              projects@crocuspearl.com
+            </a>
+            <a href="/" className="nav-privacy">
+              Privacy Policy
+            </a>
+            <div className="nav-socials">
+              <p className="sub-heading">Social</p>
+              <a
+                href="https://www.facebook.com/people/Crocus-Pearl-Technologies-Pvt-Ltd/100088528600904/?mibextid=ZbWKwL"
+                // className="link-button"
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                Facebook
               </a>
-              <a href="/" className="nav-privacy">
-                Privacy Policy
+              <a
+                href="https://www.instagram.com/crocus.pearl/"
+                // className="link-button"
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                Instagram
               </a>
-              <div className="nav-socials">
-                <p className="sub-heading">Social</p>
-                <a
-                  href="https://www.facebook.com/people/Crocus-Pearl-Technologies-Pvt-Ltd/100088528600904/?mibextid=ZbWKwL"
-                  // className="link-button"
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  Facebook
-                </a>
-                <a
-                  href="https://www.instagram.com/crocus.pearl/"
-                  // className="link-button"
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  Instagram
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/crocus-pearl-technologies-pvt-ltd/mycompany/"
-                  // className="link-button"
-                  target={"_blank"}
-                  rel={"noreferrer"}
-                >
-                  Linkedin
-                </a>
-              </div>
+              <a
+                href="https://www.linkedin.com/company/crocus-pearl-technologies-pvt-ltd/mycompany/"
+                // className="link-button"
+                target={"_blank"}
+                rel={"noreferrer"}
+              >
+                Linkedin
+              </a>
             </div>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
-export default Nav;
+export default Playground;
