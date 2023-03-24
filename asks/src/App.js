dbda4c12eb373f721bc13017f20d113d425b6713
kritydangol <kritydangol1@gmail.com>
2023-03-24 14:50:44 +0545

    CORRECTIONS

--- asks/src/App.js
+++ asks/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import AppRouting from "./utils/routing";
 
+// fonts
+import "./assets/font/GraphikLight.otf";
+import "./assets/font/GraphikExtralight.otf";
+import "./assets/font/GraphikRegular.otf";
+
 function App() {
   return <AppRouting />;
 }
--- asks/src/Pages/works.js
+++ asks/src/Pages/works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
-// import AiOutlineArrowRight from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Works = () => {
   const cardItems = [
